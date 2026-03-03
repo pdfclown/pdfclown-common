538a7eeb82da2464652f10b8bc517bda5497b43f
stechio <stechio@users.noreply.github.com>
2026-03-03 13:45:12 +0100

    refactor(build): remove temporary pdfclown-common-util code

--- pdfclown-common-build/src/main/java/org/pdfclown/common/build/util/source/JavaParsers.java
+++ pdfclown-common-build/src/main/java/org/pdfclown/common/build/util/source/JavaParsers.java
@@ -12,7 +12,7 @@
  */
 package org.pdfclown.common.build.util.source;
 
-import static org.pdfclown.common.build.internal.temp.util.Objects.enclosingTypes;
+import static org.pdfclown.common.util.Objects.enclosingTypes;
 import static org.pdfclown.common.util.Objects.opt;
 
 import com.github.javaparser.ast.CompilationUnit;
