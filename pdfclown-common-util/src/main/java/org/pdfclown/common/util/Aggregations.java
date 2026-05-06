dcc97faf309536bb4f6bd01c7b1deae8aa42ce48
stechio <stechio@users.noreply.github.com>
2026-05-06 16:15:45 +0200

    feat(util)!: remove `Aggregations::stream`
    Use `org.apache.commons.lang3.stream.Streams::of` instead
    
    BREAKING CHANGE: `Aggregations::stream` removed

--- pdfclown-common-util/src/main/java/org/pdfclown/common/util/Aggregations.java
+++ pdfclown-common-util/src/main/java/org/pdfclown/common/util/Aggregations.java
@@ -28,7 +28,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.Spliterator;
-import java.util.Spliterators;
 import java.util.WeakHashMap;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -793,13 +792,6 @@ public final class Aggregations {
       }
     }
     return target;
-  }
-
-  /**
-   * Converts the iterator into a stream.
-   */
-  public static <E extends @Nullable Object> Stream<E> stream(Iterator<E> iterator) {
-    return StreamSupport.stream(Spliterators.spliteratorUnknownSize(iterator, 0), false);
   }
 
   /**
