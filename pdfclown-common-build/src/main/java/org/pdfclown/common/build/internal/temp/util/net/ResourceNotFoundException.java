78808e3942d94be685792c784cfccc22f868b08e
stechio <stechio@users.noreply.github.com>
2026-07-28 04:51:29 +0200

    style: normalize headers

--- pdfclown-common-build/src/main/java/org/pdfclown/common/build/internal/temp/util/net/ResourceNotFoundException.java
+++ pdfclown-common-build/src/main/java/org/pdfclown/common/build/internal/temp/util/net/ResourceNotFoundException.java
@@ -3,14 +3,11 @@
 
   SPDX-License-Identifier: LGPL-3.0-only
 
-  This file (ResourceNotFoundException.java) is part of pdfclown-common-util module
-  in pdfClown
+  This file (ResourceNotFoundException.java) is part of pdfclown-common-build module in pdfClown
   Common project <https://github.com/pdfclown/pdfclown-common>
 
-  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER. If you
-  reuse (entirely or partially)
-  this file, you MUST add your own copyright notice in a separate comment block abov
-  e this file
+  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER. If you reuse (entirely or partially)
+  this file, you MUST add your own copyright notice in a separate comment block above this file
   header, listing the main changes you applied to the original source.
  */
 package org.pdfclown.common.build.internal.temp.util.net;
