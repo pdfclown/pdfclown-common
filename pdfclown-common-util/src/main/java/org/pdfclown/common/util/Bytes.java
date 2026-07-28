5e2771b8b4f8703c3d03a8d5d152cb7a7f5a33f8
stechio <stechio@users.noreply.github.com>
2026-07-28 04:14:07 +0200

    feat(util): improve `Bytes`
    Adds byte-array matching utilities (`containsAt`, `startsWith`)

--- pdfclown-common-util/src/main/java/org/pdfclown/common/util/Bytes.java
+++ pdfclown-common-util/src/main/java/org/pdfclown/common/util/Bytes.java
@@ -13,6 +13,9 @@
 package org.pdfclown.common.util;
 
 import static java.lang.System.arraycopy;
+import static java.util.Arrays.mismatch;
+import static org.pdfclown.common.util.Objects.INDEX__NOT_FOUND;
+import static org.pdfclown.common.util.Objects.equalsAny;
 
 /**
  * Byte utilities.
@@ -23,7 +26,7 @@ public class Bytes {
   public static final byte[] BYTE_ARRAY__EMPTY = new byte[0];
 
   /**
-   * Concatenates arrays.
+   * Concatenates byte arrays.
    */
   public static byte[] concat(byte[]... aa) {
     byte[] ret;
@@ -67,5 +70,26 @@ public class Bytes {
       arraycopy(a3, 0, ret, destPos, a3.length);
     }
     return ret;
+  }
+
+  /**
+   * Gets whether a byte array contains the given prefix at the given position.
+   *
+   * @param offset
+   *          {@code array} position from which to start the match.
+   */
+  public static boolean containsAt(byte[] array, int offset, byte[] prefix) {
+    return equalsAny(mismatch(array, offset, array.length, prefix, 0, prefix.length),
+        prefix.length /* `prefix` smaller than `array` over the specified range */,
+        INDEX__NOT_FOUND /* `prefix` equal to `array` over the specified range */);
+  }
+
+  /**
+   * Gets whether a byte array starts with the given prefix.
+   *
+   * @implNote This method is an analogue to {@link String#startsWith(String)}.
+   */
+  public static boolean startsWith(byte[] array, byte[] prefix) {
+    return containsAt(array, 0, prefix);
   }
 }
